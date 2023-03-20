0669406cc22febf0f5905d835b51ef90f7948183
yuto <hiura.work0404@gmail.com>
2023-03-20 14:08:05 +0900

    テーブルのサンプルを作成

---
+++ AspDotNetSampleCode/Controllers/TableController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AspDotNetSampleCode.Models;
+
+namespace AspDotNetSampleCode.Controllers
+{
+    /// <summary>
+    /// テーブル用のコントローラクラスです。
+    /// </summary>
+    public class TableController : Controller
+    {
+        /// <summary>
+        /// 画面を作成します。
+        /// </summary>
+        /// <returns>作成された画面を返します。</returns>
+        public IActionResult Index()
+        {
+            List<TableDataModel> datas = GetTableDatas();
+            ViewBag.Datas = datas;
+
+            string yourName = "bbbbb";
+            ViewData["mame"] = yourName;
+
+            return View(datas);
+        }
+
+        private List<TableDataModel> GetTableDatas()
+        {
+            List<TableDataModel> datas = new List<TableDataModel>();
+
+            for (int i = 0; i < 50; i++)
+            {
+                TableDataModel data = new TableDataModel();
+                data.Name = "テスト　太郎";
+                data.Age = "30";
+                data.Gender = "男";
+                data.PhoneNumber = "000-0000-0000";
+                datas.Add(data);
+            }
+
+            return datas;
+        }
+    }
+}
---
+++ AspDotNetSampleCode/Models/TableDataModel.cs
@@ -0,0 +1,28 @@
+﻿namespace AspDotNetSampleCode.Models
+{
+    /// <summary>
+    /// テーブルデータのモデルクラスです。
+    /// </summary>
+    public class TableDataModel
+    {
+        /// <summary>
+        /// 名前です。
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// 年齢です。
+        /// </summary>
+        public string? Age { get; set; }
+
+        /// <summary>
+        /// 性別です。
+        /// </summary>
+        public string? Gender { get; set; }
+
+        /// <summary>
+        /// 電話番号です。
+        /// </summary>
+        public string? PhoneNumber { get; set; }
+    }
+}
